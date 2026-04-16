53729a7e2abc8d19ebfc4729fb7eb45b5f59e90c
MateoBernardi <mateo.bernardi@mi.unc.edu.ar>
2026-04-16 20:12:19 -0300

    feat: Add implementation processing with Python and assembly integration

---
+++ tp2_stack_frame/connect_py_with_asm.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Declaración de la función en assembler
+extern int procesar_gini_via_stack(float valor);
+
+int process_gini(float valor)
+{
+    return procesar_gini_via_stack(valor);
+}
